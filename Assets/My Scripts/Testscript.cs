12dd9d658db2f30fb958f0f0c4e88957b7a61434
SubstrateZero <141806973+SubstrateZero@users.noreply.github.com>
2025-08-18 10:22:32 +0200

    Fix

--- Assets/My Scripts/Testscript.cs
+++ Assets/My Scripts/Testscript.cs
@@ -5,7 +5,7 @@ public class Testscript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hallo")
+        Debug.Log("Hallo");
     }
 
     // Update is called once per frame
